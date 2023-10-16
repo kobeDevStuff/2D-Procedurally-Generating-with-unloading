6dd2c95b90dbf4047636d350ff4fe2f6a939fb5f
KobeDev <kobe.dellafavory@gmail.com>
2023-10-16 16:44:55 +0800

    Main Code
    Just import this to test for yourself if you want to or you can copy and paste the scripts and follow along with the tutorial.
    Credit to @SlothInTheHat for the Tilemap code but I have improved and revised some bugs that also adds a chunk unloader.
    
    Free top down smooth player movement too included.
    
    Co-Authored-By: SlothInTheHat <71303537+slothinthehat@users.noreply.github.com>

---
+++ player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const ACCEL = 2
+
+var input: Vector2
+
+func get_input():
+	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	return input.normalized()
+
+func _process(delta):
+	var playerInput = get_input()
+	
+	velocity = lerp(velocity, playerInput * SPEED, delta * ACCEL)
+	
+	move_and_slide()
+
---
+++ tilemap.gd
@@ -0,0 +1,77 @@
+extends TileMap
+
+# Initialize noise generators for different map features
+# Values between -1 and 1
+var moisture = FastNoiseLite.new()
+var temperature = FastNoiseLite.new()
+var altitude = FastNoiseLite.new()
+
+# Dimensions of each generated chunk
+var width = 64
+var height = 64
+
+# Reference to the player character
+@onready var player = get_parent().get_node("Player")
+
+# List to keep track of loaded chunks
+var loaded_chunks = []
+
+func _ready():
+	# Set random seeds for noise variation
+	moisture.seed = randi()
+	temperature.seed = randi()
+	altitude.seed = randi()
+	
+	# Adjust this value to change the 'smoothness' of the map; lower values mean more smooth noise
+	altitude.frequency = 0.01
+
+func _process(delta):
+	# Convert the player's position to tile coordinates
+	var player_tile_pos = local_to_map(player.position)
+	
+	# Generate the chunk at the player's position
+	generate_chunk(player_tile_pos)
+	
+	# Unload chunks that are too far away.
+	# Note: Not needed for smaller projects but if you are loading a bigger tilemap it's good practice
+	unload_distant_chunks(player_tile_pos)
+
+
+func generate_chunk(pos):
+	for x in range(width):
+		for y in range(height):
+			# Generate noise values for moisture, temperature, and altitude
+			var moist = moisture.get_noise_2d(pos.x - (width/2) + x, pos.y - (height/2) + y) * 10 # Values between -10 and 10
+			var temp = temperature.get_noise_2d(pos.x - (width/2) + x, pos.y - (height/2) + y) * 10
+			var alt = altitude.get_noise_2d(pos.x - (width/2) + x, pos.y - (height/2) + y) * 10
+
+			# Set the cell based on altitude; adjust for different tile types
+			# Need to evenly distribute -10 -> 10 to 0 -> 4....  This can be done by first adding 10
+			# Gets values from 0 -> 20... Then we will multiply by 3/20 in order to remap it to 0 -> 3
+			set_cell(0, Vector2i(pos.x - (width/2) + x, pos.y - (height/2) + y), 0, Vector2(round(3 * (moist + 10) / 20), round(3 * (temp + 10) / 20)))
+			if Vector2i(pos.x, pos.y) not in loaded_chunks:
+				loaded_chunks.append(Vector2i(pos.x, pos.y))
+
+# Function to unload chunks that are too far away
+func unload_distant_chunks(player_pos):
+	# Set the distance threshold to at least 2 times the width to limit visual glitches
+	# Higher values unload chunks further away
+	var unload_distance_threshold = (width * 2) + 1
+
+	for chunk in loaded_chunks:
+		var distance_to_player = get_dist(chunk, player_pos)
+
+		if distance_to_player > unload_distance_threshold:
+			clear_chunk(chunk)
+			loaded_chunks.erase(chunk)
+
+# Function to clear a chunk
+func clear_chunk(pos):
+	for x in range(width):
+		for y in range(height):
+			set_cell(0, Vector2i(pos.x - (width/2) + x, pos.y - (height/2) + y), -1, Vector2(-1, -1), -1)
+
+# Function to calculate distance between two vectors
+func get_dist(vec1, vec2):
+	var resultant = vec2 - vec1
+	return sqrt(resultant.x ** 2 + resultant.y ** 2)
