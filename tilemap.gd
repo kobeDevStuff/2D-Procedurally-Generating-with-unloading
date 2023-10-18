93bd583b116969a461b23ce502201eda5fbf08d3
KobeDev <kobe.dellafavory@gmail.com>
2023-10-18 18:13:22 +0800

    Updating Code
    Here is the majority of code you need for your own project

--- tilemap.gd
+++ tilemap.gd
@@ -21,17 +21,17 @@ func _ready():
 	moisture.seed = randi()
 	temperature.seed = randi()
 	altitude.seed = randi()
-	
+
 	# Adjust this value to change the 'smoothness' of the map; lower values mean more smooth noise
 	altitude.frequency = 0.01
 
 func _process(delta):
 	# Convert the player's position to tile coordinates
 	var player_tile_pos = local_to_map(player.position)
-	
+
 	# Generate the chunk at the player's position
 	generate_chunk(player_tile_pos)
-	
+
 	# Unload chunks that are too far away.
 	# Note: Not needed for smaller projects but if you are loading a bigger tilemap it's good practice
 	unload_distant_chunks(player_tile_pos)
@@ -48,7 +48,14 @@ func generate_chunk(pos):
 			# Set the cell based on altitude; adjust for different tile types
 			# Need to evenly distribute -10 -> 10 to 0 -> 4....  This can be done by first adding 10
 			# Gets values from 0 -> 20... Then we will multiply by 3/20 in order to remap it to 0 -> 3
-			set_cell(0, Vector2i(pos.x - (width/2) + x, pos.y - (height/2) + y), 0, Vector2(round(3 * (moist + 10) / 20), round(3 * (temp + 10) / 20)))
+			# vvv
+
+			if alt < 0: # Arbitrary sea level value (choosing 0 will mean roughly 1/2 the world is ocean)
+				set_cell(0, Vector2i(pos.x - (width/2) + x, pos.y - (height/2) + y), 0, Vector2(3, round(3 * (temp + 10) / 20))) # Change x value where I've wrote three to whatever the x-coord of your oceans are
+			else: # You can add other logic like making beaches by setting x-coord to whatever beach atlas x-coord is when the alt is between 0 and 0.5 or something
+				set_cell(0, Vector2i(pos.x - (width/2) + x, pos.y - (height/2) + y), 0, Vector2(round(3 * (moist + 10) / 20), round(3 * (temp + 10) / 20)))
+			
+			
 			if Vector2i(pos.x, pos.y) not in loaded_chunks:
 				loaded_chunks.append(Vector2i(pos.x, pos.y))
 
@@ -71,7 +78,7 @@ func clear_chunk(pos):
 		for y in range(height):
 			set_cell(0, Vector2i(pos.x - (width/2) + x, pos.y - (height/2) + y), -1, Vector2(-1, -1), -1)
 
-# Function to calculate distance between two vectors
-func get_dist(vec1, vec2):
-	var resultant = vec2 - vec1
+# Function to calculate distance between two points
+func get_dist(p1, p2):
+	var resultant = p1 - p2
 	return sqrt(resultant.x ** 2 + resultant.y ** 2)
